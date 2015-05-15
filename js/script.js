504d287ffa61980c3dd34dc28cc044e441cb7536
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-14 19:42:48 -0600

    Web Socket Example

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
 	console.log("Hello, World!")
+	
+	
+	var ex = new WebSocket('ws://127.0.0.1:3555')
+	
+	// global.WebSocket API (not the node API)
+	ex.onopen = function (event) {
+		ex.send('hi')
+	}
+	
+	ex.onmessage = function (event) {
+		console.log(event.data)
+		ex.send('hi')
+	}
+	
 });
